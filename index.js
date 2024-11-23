8d081ba1ba0a9f5a8884c5d406086a79f21499bf
Matheqs <matheqs@outlook.com>
2024-11-23 03:14:09 -0300

    Server initial configutation

--- index.js
+++ index.js
@@ -2,3 +2,15 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+//server configuration
+const app = express();
+const port = 3000;
+const API_URL = "https://pokeapi.co/api/v2/pokemon/";
+// public folder and bodyParser set.
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//listen function, always in the end
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
